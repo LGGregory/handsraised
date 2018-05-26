74c42f5722ef38825696f8c563a1826437a98dcd
LGGregory <liam@euclid.ca>
2018-05-26 02:14:46 -0400

    Progress for db access

--- app.js
+++ app.js
@@ -1,49 +1,74 @@
 'use strict';
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var mongoURL = 'mongodb://handsraised:RUHacks2018@ds135750.mlab.com:35750/handsraised';
+app.use(bodyParser.urlencoded({extended: true}));
 
+var MongoClient = require('mongodb').MongoClient;
 var port = process.env.PORT || 3000;
+var db;
+MongoClient.connect(mongoURL, function (err, client) {
+    if (err)
+        return console.log(err);
+    db = client.db('handsraised');
 
-app.listen(port, function(){
-  console.log("server is running on port " + port);
-});
-
-app.get('/', function(req, res){
-  res.send("<h1>Default page</h1>");
-});
-
-app.get('/:dateParam', function(req, res){
-  var dateParam = req.params.dateParam;
-
-  if(Number(dateParam)){
-    var date = "3";
-
-    if(date != "Invalid date"){
-      res.json({
-        unix: dateParam,
-        natural: date
+    app.listen(port, function () {
+        console.log("server is running on port " + port);
     });
-    } else{
-      res.json({
-        unix: null,
-        natural: null
-    });
-    }
-  }
 
-  else{
-    var date = "3";
-    if(date){
-      res.json({
-        unix: date.unix(),
-        natural: date.format("MMMM D, YYYY")
-      });
-    }
-    else{
-      res.json({
-        unix: null,
-        natural: null
-      });
-    }
-  }
 });
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.post('/join_session', function (req, res) {
+
+    console.log(req.body);
+
+
+
+});
+
+
+
+/*
+ 
+ app.get('/:dateParam', function(req, res){
+ var dateParam = req.params.dateParam;
+ 
+ if(Number(dateParam)){
+ var date = "3";
+ 
+ if(date != "Invalid date"){
+ res.json({
+ unix: dateParam,
+ natural: date
+ });
+ } else{
+ res.json({
+ unix: null,
+ natural: null
+ });
+ }
+ }
+ 
+ else{
+ var date = "3";
+ if(date){
+ res.json({
+ unix: date.unix(),
+ natural: date.format("MMMM D, YYYY")
+ });
+ }
+ else{
+ res.json({
+ unix: null,
+ natural: null
+ });
+ }
+ }
+ });
+ */
