9fe17dbe8f80950b126ca991741852bbee0b36cd
LGGregory <liam@euclid.ca>
2018-05-26 13:21:14 -0400

    db querying

--- app.js
+++ app.js
@@ -26,16 +26,48 @@ app.get('/', function (req, res) {
 app.get('/create.html', function (req, res) {
     res.sendFile(__dirname + '/views/create.html');
 });
+
+app.get('/session.html', function (req, res) {
+    res.sendFile(__dirname + '/views/create.html');
+});
+
+app.get('/create.html', function (req, res) {
+    res.sendFile(__dirname + '/views/create.html');
+});
+
 app.post('/join_session', function (req, res) {
+    var cursor = db.collection('session_keys').find();
+
     res.sendFile(__dirname + '/views/raise.html');
 });
 
-
 app.post('/create_session', function (req, res) {
-    res.sendFile(__dirname + '/views/session.html');
+    db.collection('session_keys').save(req.body, function (err, result) {
+        if (err)
+            return console.log(err);
+        console.log('saved to database');
+    });
+
+    var query = { session_name: "First" };
+    
+    db.collection('session_keys').find(query).toArray(function (err, res) {
+        if (err)
+            return console.log(err);
+        console.log(res);
+    });
+    
+    res.redirect('/session.html');
+        res.sendFile(__dirname + '/views/session.html');
 });
 
-
+app.get('/session.html', function (req, res) {
+    res.sendFile(__dirname + '/views/session.html');
+    db.collection('session_keys').find().toArray(function (err, res) {
+        if (err)
+            return console.log(err);
+        console.log(res);
+    });
+});
 
 /*
  
