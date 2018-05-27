fde8f52c4099b79e642ce6d009540ba9382ec612
esraa-saleh <esraa.saleh.carleton@gmail.com>
2018-05-27 01:36:02 -0400

    dummy python scrit update

--- LDATest.py
+++ LDATest.py
@@ -5,9 +5,8 @@ from sklearn.decomposition import NMF, LatentDirichletAllocation
 
 def display_topics(model, feature_names, no_top_words):
     for topic_idx, topic in enumerate(model.components_):
-        print "Topic %d:" % (topic_idx)
-        print " ".join([feature_names[i]
-                        for i in topic.argsort()[:-no_top_words - 1:-1]])
+        print( "Topic %d:" % (topic_idx))
+        print(" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
 
 dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
 documents = dataset.data
